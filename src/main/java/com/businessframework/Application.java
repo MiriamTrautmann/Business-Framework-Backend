1fae0619efea6a975f8a94d0afa608a30d0ef8fd
Miriam Trautmann <miriam-trautmann@gmx.de>
2022-02-16 17:33:26 +0100

    Final Version

--- src/main/java/com/businessframework/Application.java
+++ src/main/java/com/businessframework/Application.java
@@ -18,6 +18,5 @@ public class Application {
     public static void main (String[] args) throws UnirestException, UnsupportedEncodingException {
         SpringApplication.run(Application.class, args);
 
-
     }
 }
